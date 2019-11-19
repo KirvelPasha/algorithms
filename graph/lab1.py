d05e5038745629c775c0eadea0301e50b84c9324
Pasha <kirvelpasha99@gmail.com>
2019-11-19 22:03:13 +0300

    update

--- graph/lab1.py
+++ graph/lab1.py
@@ -1,43 +1,98 @@
 class Graph:
 
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
         self.matrix = []
 
     def adjacencyMatrix(self):
-        for i in range(self.size):
+        for i in range(self.vertices):
             self.matrix.append([])
-            for j in range(self.size):
-                value = int(input())
-                if value == 0 or value == 1:
-                    self.matrix[i].append(value)
+            for j in range(self.vertices):
+                self.matrix[i].append(0)
+        for i in range(self.edges):
+            print("Введите номера смежных вершин")
+            u = int(input())
+            v = int(input())
+            u = u - 1
+            v = v - 1
+            self.matrix[u][v] = self.matrix[v][u] = 1
         print("Матрица смежности:")
         return self.matrix
 
     def adjacencyList(self):
         adjacencyList = {}
-        for i in range(self.size):
+        for i in range(self.vertices):
             adjacencyList.setdefault(i, [])
-            for j in range(self.size):
+            for j in range(self.vertices):
                 if self.matrix[i][j] == 1:
                     adjacencyList.get(i).append(j)
         return adjacencyList
 
     def incidenceMatrix(self):
-
+        # столбцы матрицы ребра
+        # строки вершины
+        incidenceMatrix = []
+        for i in range(self.vertices):
+            incidenceMatrix.append([])
+            for j in range(self.edges):
+                incidenceMatrix[i].append(0)
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                if self.matrix[i][j] == 1:
+                    incidenceMatrix[i].append(1)
         return
 
     def listOfArcs(self):
-
         return
 
+    def addNote(self):
+        for i in range(self.vertices):
+            self.matrix[i].append(0)
+        self.vertices = self.vertices + 1
+        newNote = []
+        for i in range(self.vertices):
+            newNote.append(0)
+        self.matrix.append(newNote)
+        return self.matrix
+
+    def deleteNote(self):
+        self.vertices = self.vertices - 1
+        v = int(input("Введите номер вершины")) - 1
+        self.matrix.pop(v)
+        for i in range(self.vertices):
+            self.matrix[i].pop(v)
+        return self.matrix
+
+    def addArc(self):
+        self.edges = self.edges + 1
+        u = int(input("Введите номер вершины")) - 1
+        v = int(input("Введите номер вершины")) - 1
+        self.matrix[u][v] = self.matrix[v][u] = 1
+        return self.matrix
+
+    def deleteArc(self):
+        self.edges = self.edges - 1
+        u = int(input("Введите номер вершины")) - 1
+        v = int(input("Введите номер вершины")) - 1
+        self.matrix[u][v] = 0
+        self.matrix[v][u] = 0
+        return self.matrix
+
+    def printGraph(self):
+        return self.matrix
 
 
 def main():
-    size = int(input("Введите размер матрицы:"))
-    graph = Graph(size)
+    vertices = int(input("Введите количество вершин"))
+    edges = int(input("Введите количество ребер"))
+    graph = Graph(vertices, edges)
     print(graph.adjacencyMatrix())
     print(graph.adjacencyList())
+    print(graph.addNote())
+    print(graph.deleteNote())
+    print(graph.addArc())
+    print(graph.deleteArc())
 
 
 if __name__ == "__main__":
