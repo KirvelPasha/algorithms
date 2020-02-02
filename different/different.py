651aca5a15fb452f05a152b3e21dd5a229b3c2fb
Pasha <kirvelpasha99@gmail.com>
2020-02-02 18:24:11 +0300

    Estimate pi

--- different/different.py
+++ different/different.py
@@ -1,3 +1,6 @@
+import random
+
+
 # A simple program
 # to find three elements whose
 # sum is equal to zero
@@ -26,14 +29,26 @@ def findTripletsH(arr):
         print("No")
 
 
-def subArraySum(arr, sum):
-    length = len(arr)
+# Estimate pi, given that you have random (0, 1)
+def estimate_pi(n):
+    num_point_circle = 0
+    num_point_total = 0
+    for i in range(n):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        distance = x ** 2 + y ** 2
+        if distance <= 1:
+            num_point_circle += 1
+        num_point_total += 1
+
+    return 4 * num_point_circle / num_point_total
 
 
 def main():
-    arr = [0, -1, 2, -3, 1]
+    # arr = [0, -1, 2, -3, 1]
     # findTriplets(arr)
     # findTripletsH(arr)
+    print(estimate_pi(1000000))
 
 
 if __name__ == "__main__":
